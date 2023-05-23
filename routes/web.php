92868f6324b0202cbebb9a06dd60a7676a868c74
David López <dalo_vidpez@hotmail.com.ar>
2023-05-23 14:01:46 -0300

    Arreglado middleware

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('productos', ProductoController::class);
+
+    Route::resource('categorias', CategoriaController::class);
+
+    Route::get('/clientes', [ClienteController::class,'index'])->name('clientes');
+
+    Route::get('/compras', [CompraController::class, 'index'])->name('compras');
 });
-
-Route::resource('productos', ProductoController::class);
-
-Route::resource('categorias', CategoriaController::class);
-
-Route::get('/clientes', [ClienteController::class,'index'])->name('clientes');
-
-Route::get('/compras', [CompraController::class, 'index'])->name('compras');
 
 require __DIR__.'/auth.php';
