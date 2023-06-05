9732cacd5336a938014c57ebf9151adf5cb1a68b
David López <dalo_vidpez@hotmail.com.ar>
2023-06-05 12:37:24 -0300

    Borrado js innecesario

--- public/js/form.js
+++
@@ -1,8 +0,0 @@
-function validarTalles(talles_validos, talles_actual){
-    talles = talles_actual.toUpperCase();
-    talles = talles.replaceAll(/\s/, '');
-    
-    console.log(talles_validos.includes(talles));
-
-    return talles_validos.includes(talles);
-}
