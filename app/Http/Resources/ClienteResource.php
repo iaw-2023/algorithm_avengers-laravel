b3b7c4490b8e0643320362bdd3c1c6d538b91a18
David López <dalo_vidpez@hotmail.com.ar>
2023-06-06 14:44:14 -0300

    Documentado en L5-Swagger post de clientes

--- app/Http/Resources/ClienteResource.php
+++ app/Http/Resources/ClienteResource.php
@@ -22,7 +22,63 @@ class ClienteResource extends JsonResource
      *          response="default",
      *          description="Error inesperado"
      *      )
+     * ),
+     * 
+     * @OA\Post(
+     *      tags={"clientes"},
+     *      path="/rest/clientes",
+     *      summary="Almacena un nuevo cliente en el sistema",
+     *      description="Almacena un nuevo cliente en el sistema y lo retorna",
+     *      @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     example=2
+     *                ),
+     *                @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="email",
+     *                     example="juan_gonzález@example.com"
+     *                ),
+     *                @OA\Property(
+     *                     property="contrasena",
+     *                     type="string",
+     *                     example="2af329f4923f1ccde3cbaeb949e3fe32",
+     *                     description="Contraseña codificada en md5"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="nombre",
+     *                     type="string",
+     *                     example="Juan González"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="telefono",
+     *                     type="string",
+     *                     example="+54 1 111 111 1111"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="domicilio",
+     *                     type="string",
+     *                     example="San Martín 1810"
+     *                 )  
+     *             )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="200",
+     *          description="Operación realizada con éxito",
+     *          @OA\JsonContent(ref="#/components/schemas/Cliente")
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="Error inesperado"
+     *      )
      * )
+     * 
      */
     public function toArray(Request $request): array
     {
