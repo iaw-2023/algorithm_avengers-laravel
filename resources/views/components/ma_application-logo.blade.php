30072bcb409e4887008f40b88eec38e8fc1a8d45
David López <dalo_vidpez@hotmail.com.ar>
2024-08-02 11:57:54 -0300

    Fixed page images

--- resources/views/components/ma_application-logo.blade.php
+++ resources/views/components/ma_application-logo.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/componentes.css') }}">
 
-<img id="logo" src="https://manosargentinas.com/inicio/wp-content/uploads/2022/04/logo_new1.png">
+<img id="logo" src="{{ asset('images/ma_white.png')}}">
