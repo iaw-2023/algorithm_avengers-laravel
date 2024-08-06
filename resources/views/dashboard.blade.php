8d9a6559840cad492b0ccf1d5b60794cf9a883c2
David López <dalo_vidpez@hotmail.com.ar>
2024-08-06 11:15:59 -0300

    Added margin to navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,4 @@
 <x-ma_layout>
-    <x-slot name="header">
-        <div class="container-lg">
-            <h2>
-                {{ __('Dashboard') }}
-            </h2>   
-        </div>
-    </x-slot>
-
     <div class="container-sm">
         <h4>
             <div>¡Bienvenido {{ Auth::user()->name }}!</div>
