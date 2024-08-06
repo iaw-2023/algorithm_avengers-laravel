8a909187bd2948621728709169aee8e0f78c2950
David López <dalo_vidpez@hotmail.com.ar>
2024-08-06 18:16:59 -0300

    Changed way to set margin in the layout (using Boostrap margin)

--- resources/views/components/ma_layout.blade.php
+++ resources/views/components/ma_layout.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/layout.css') }}">
 
         <title>{{ config('app.name', 'Manos Argentinas - Tienda Online') }}</title>
         
@@ -25,7 +24,7 @@
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="m-2">
                 {{ $slot }}
             </main>
         </div>
