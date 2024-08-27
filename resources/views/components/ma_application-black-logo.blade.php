e5319e7a968b80860b170982cfc03279d95c0254
David López <dalo_vidpez@hotmail.com.ar>
2024-08-27 10:40:40 -0300

    Replaced web page local logos with Cloudinary logos

--- resources/views/components/ma_application-black-logo.blade.php
+++ resources/views/components/ma_application-black-logo.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/componentes.css') }}">
 
-<img id="logo" src="{{ asset('images/ma_black.png')}}">
+<img id="logo" src="https://res.cloudinary.com/drspuruy2/image/upload/v1724764144/ma_black_ejrpeo.png">
