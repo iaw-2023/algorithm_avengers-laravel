f694ac8709079e060109d6c3db5ced8bb3667dda
David López <dalo_vidpez@hotmail.com.ar>
2025-01-07 11:39:03 -0300

    Harcoded Categorias id

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -15,18 +15,22 @@ class CategoriaSeeder extends Seeder
     public function run(): void
     {
         DB::table('categorias')->insert([
+            'id' => 1,
             'nombre' => 'Masculinidades',
             'descripcion' => 'Ropa diseñada para masculinidades'
         ]);
         DB::table('categorias')->insert([
+            'id' => 2,
             'nombre' => 'Femenidades',
             'descripcion' => 'Ropa diseñada para feminidades'
         ]);
         DB::table('categorias')->insert([
+            'id' => 3,
             'nombre' => 'Unisex',
             'descripcion' => 'Ropa diseñada sin género específico'
         ]);
         DB::table('categorias')->insert([
+            'id' => 4,
             'nombre' => 'Infancias',
             'descripcion' => 'Ropa especialmente diseñada para niños y niñas'
         ]);
