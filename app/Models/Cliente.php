b24e5cca1b918c86f5e279cc6561e3502e121c25
David López <dalo_vidpez@hotmail.com.ar>
2025-02-24 19:42:29 -0300

    Fixed imports in authentication model

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 /**
  * @OA\Schema(
  *      schema="Cliente",
