3d0617e26d1c7328c6b0ec3c9a058ba3ba23121e
David López <dalo_vidpez@hotmail.com.ar>
2025-03-17 15:46:44 -0300

    Hidded access to compras via API without loggin

--- routes/api.php
+++ routes/api.php
@@ -45,13 +45,13 @@ Route::get('productos/{id}', function(string $id){
 });
 
 // Compras
-Route::get('compras/{id}', function(string $id){
+/* Route::get('compras/{id}', function(string $id){
     return new CompraResource(Compra::findOrFail($id));
 });
 Route::get('compras', function(){
     return CompraResource::collection(Compra::all());
 });
-Route::post('compras', [CompraController::class, 'storeAPI']);
+Route::post('compras', [CompraController::class, 'storeAPI']); */
 
 // Clientes
 /* Route::get('clientes/{id}', function(string $id){
