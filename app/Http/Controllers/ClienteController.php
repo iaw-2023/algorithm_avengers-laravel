d1250758232b60f2c16ec9107dc7d3ae0e0dae87
David López <dalo_vidpez@hotmail.com.ar>
2025-04-11 16:22:00 -0300

    Fixed typo error: producto instead productos in CLienteController

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -105,7 +105,7 @@ class ClienteController extends Controller
     public function profile(Request $request){
         $user = auth()->user()->makeHidden(['contrasena']);
         $cliente = Cliente::where('email', $user->email)
-            ->with(['compras.detalles.productos'])
+            ->with(['compras.detalles.producto'])
             ->first();
 
         $cliente->makeHidden(['contrasena']);
