5a837bf5df3629c69f25e01ed5ac5e88fca05682
David López <dalo_vidpez@hotmail.com.ar>
2025-05-28 18:46:20 -0300

    Descommented code in MergadoPagoController.initializePayment

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -23,6 +23,12 @@ class MercadoPagoController extends Controller{
             $preference = $client->create([
                 "items" => $request->items,
                 "payer" => $request->payer,
+                /* "back_urls" => [
+                    'success' => "${frontendUrl}/success",
+                    'failure' => "{$frontendUrl}/failure",
+                    'pending' => "{$frontendUrl}/pending"
+                ],
+                "auto_return" => "approved", */
             ]);
             return response()->json([
                 'id' => $preference->id,
